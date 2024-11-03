408f6fa0f77e4bd0483fc2bfc365d15afec2c1ad
Gaston Zarate <gaston.z195@gmail.com>
2024-11-03 14:39:38 -0300

    refactor: Remove NavigationLinks component and update NavigationBar

--- src/app/bots/NavigationBar.tsx
+++ src/app/bots/NavigationBar.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link';
 
-import NavigationLinks from '@/app/bots/NavigationLinks';
 import ThemeSwitch from '@/app/bots/ThemeSwitch';
 
 const NavigationBar = () => {
     return (
         <div className='mx-auto mt-3 flex w-full max-w-7xl flex-col-reverse items-center justify-between gap-6 px-3 sm:flex-row sm:px-0 lg:mt-6'>
-            <NavigationLinks />
             <div className='flex w-full justify-between gap-6 sm:w-auto sm:items-center'>
                 <ThemeSwitch />
             </div>
