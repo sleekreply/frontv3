224feb2a38ee73cdc63366a6aa9979a40255788f
Gaston Zarate <gaston.z195@gmail.com>
2024-11-03 21:07:44 -0300

    refactor: Update sidebar component structure and styling; enhance theme switch layout

--- src/app/dashboard/components/app-sidebar.tsx
+++ src/app/dashboard/components/app-sidebar.tsx
@@ -4,20 +4,56 @@ import {
     SidebarFooter,
     SidebarGroup,
     SidebarHeader,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+    SidebarGroupContent
   } from "@/components/ui/sidebar"
+import { Bot, MessageCircleCode } from "lucide-react"
 import ThemeSwitch from "./theme-switch"
 
-  export function AppSidebar() {
+const items = [
+    {
+      title: "Bots",
+      url: "#",
+      icon: Bot,
+    },
+    {
+        title: "Messages",
+        url: "#",
+        icon: MessageCircleCode,
+    },
+]
+
+export function AppSidebar() {
     return (
-      <Sidebar>
+        <Sidebar>
         <SidebarHeader />
-        <SidebarContent>
-          <SidebarGroup />
-          <SidebarGroup />
-        </SidebarContent>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                        {items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={item.url}>
+                                <item.icon />
+                                <span>{item.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        <div className='items-center'>
         <ThemeSwitch>
         </ThemeSwitch>
+        </div>
         <SidebarFooter />
-      </Sidebar>
+        </Sidebar>
     )
-  }
+}
